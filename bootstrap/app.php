26a3c3f49e00596ade0760e7b9b8b60cf9804b2b
denshernchingware <densher.n.chingware@gmail.com>
2026-05-03 22:24:34 +0530

    vercel setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,40 @@
 <?php
 
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\HandleAppearance;
+use App\Http\Middleware\HandleInertiaRequests;
+use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
-    )
+    )->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->encryptCookies(except: ['appearance', 'sidebar']);
+
+        $middleware->web(append: [
+            // HandleAppearance::class,
+            // HandleInertiaRequests::class,
+            AddLinkHeadersForPreloadedAssets::class,
+        ]);
+
+        $middleware->trustProxies(
+            '*',
+            Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO
+        );
+    })
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
